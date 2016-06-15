40e80e9f67f45d87da055088b2aeb32061a86177
wjc133 <wjc133@sina.com>
2016-06-15 20:47:05 +0800

    Initial commit

---
+++ markfox-client/src/main/java/com/elite/tools/markfox/client/bootstrap/BootStrap.java
@@ -0,0 +1,12 @@
+package com.elite.tools.markfox.client.bootstrap;
+
+/**
+ * Created by wjc133
+ * Date: 2016/6/15
+ * Time: 20:38
+ */
+public class BootStrap {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ markfox-uploader/src/main/java/com/elite/tools/markfox/uploader/Uploader.java
@@ -0,0 +1,9 @@
+package com.elite.tools.markfox.uploader;
+
+/**
+ * Created by wjc133
+ * Date: 2016/6/15
+ * Time: 20:40
+ */
+public interface Uploader {
+}
